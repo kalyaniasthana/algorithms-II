c437202992f6dbc02d8ceaefe12afce840192ae1
Kalyani Asthana <kalyaniasthana@gmail.com>
2020-06-08 16:14:31 +0530

    topological sort using dfs (no recursive)

--- depth_first_search.py
+++ depth_first_search.py
@@ -43,11 +43,58 @@ def call_dfs_recursive(G, start_node):
 
 	return sequence
 
+def indegree_graph(G):
+
+	indeg = {}
+
+	for node in G:
+		if node not in indeg:
+			indeg[node] = 0
+
+		l = G[node]
+		for node in l:
+			if node not in indeg:
+				indeg[node] = 0
+
+
+	for node in indeg:
+		count_in_node = 0
+		for node_ in G:
+			l = G[node_]
+			if node in l:
+				count_in_node += 1
+		indeg[node] = count_in_node
+
+	return indeg
+
+def topological_sort(G, indeg):
+
+	stack = []
+	topological_order = []
+
+	for node in indeg:
+		if indeg[node] == 0 and node not in stack:
+			stack.append(node)
+
+	#dfs
+	while len(stack) > 0:
+		u = stack.pop()
+		topological_order.append(u)
+		if u in G:
+			for node in G[u]:
+				indeg[node] -= 1
+				if indeg[node] == 0:
+					stack.append(node)
+
+	return topological_order
+
 '''
 filename = 'SCC.txt'
 graph = read_graph(filename)
 #start_node = choice(list(graph.keys()))
-start_node = 2
-print(call_dfs_recursive(graph, start_node))
+#start_node = 2
+#print(call_dfs_recursive(graph, start_node))
+indeg = indegree_graph(graph)
+print(topological_sort(graph, indeg))
 '''
 
